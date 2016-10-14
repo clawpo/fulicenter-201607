2bd0a0127df95710973fc9441b55553486232082
耿小泰 <clawpo@foxmail.com>
2016-10-14 16:30:15 +0800

    修改闪屏界面进入主页面没有动画的bug

--- app/src/main/java/cn/ucai/fulicenter/activity/SplashActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/SplashActivity.java
@@ -1,6 +1,7 @@
 package cn.ucai.fulicenter.activity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import cn.ucai.fulicenter.R;
@@ -19,23 +20,12 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        new Thread(new Runnable() {
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
-                long start = System.currentTimeMillis();
-                //create db
-                long costTime = System.currentTimeMillis() - start;
-                if(sleepTime-costTime>0){
-                    try {
-                        Thread.sleep(sleepTime-costTime);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
                 MFGT.gotoMainActivity(SplashActivity.this);
-                MFGT.finish(SplashActivity.this);
+                finish();
             }
-        }).start();
+        },sleepTime);
     }
 }
