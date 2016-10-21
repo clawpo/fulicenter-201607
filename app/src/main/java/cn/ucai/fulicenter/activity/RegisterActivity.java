d96a736151c9f33eba044208d7c6e10997747d12
耿小泰 <clawpo@foxmail.com>
2016-10-21 11:13:51 +0800

    添加注册页面,并绑定登录页面的跳转

---
+++ app/src/main/java/cn/ucai/fulicenter/activity/RegisterActivity.java
@@ -0,0 +1,29 @@
+package cn.ucai.fulicenter.activity;
+
+import android.os.Bundle;
+
+import cn.ucai.fulicenter.R;
+
+public class RegisterActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+}
