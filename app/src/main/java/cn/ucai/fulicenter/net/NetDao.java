8c227e8baaa6a76b38983b82e9786ad9a6299c0e
耿小泰 <clawpo@foxmail.com>
2016-10-21 17:55:29 +0800

    用户登录的逻辑处理

--- app/src/main/java/cn/ucai/fulicenter/net/NetDao.java
+++ app/src/main/java/cn/ucai/fulicenter/net/NetDao.java
@@ -72,4 +72,13 @@ public class NetDao {
                 .post()
                 .execute(listener);
     }
+
+    public static void login(Context context, String username, String password, OkHttpUtils.OnCompleteListener<Result> listener){
+        OkHttpUtils<Result> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_LOGIN)
+                .addParam(I.User.USER_NAME,username)
+                .addParam(I.User.PASSWORD,MD5.getMessageDigest(password))
+                .targetClass(Result.class)
+                .execute(listener);
+    }
 }
