2f43b044f0a84354389a75f9441fdcf2f2164306
耿小泰 <clawpo@foxmail.com>
2016-10-21 11:26:18 +0800

    添加注册页面并绑定到登录页面的点击事件

--- app/src/main/java/cn/ucai/fulicenter/activity/RegisterActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/RegisterActivity.java
@@ -1,20 +1,37 @@
 package cn.ucai.fulicenter.activity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.view.DisplayUtils;
 
 public class RegisterActivity extends BaseActivity {
 
+    @BindView(R.id.username)
+    EditText mUsername;
+    @BindView(R.id.nick)
+    EditText mNick;
+    @BindView(R.id.password)
+    EditText mPassword;
+    @BindView(R.id.confirm_password)
+    EditText mConfirmPassword;
+    @BindView(R.id.btn_register)
+    Button mBtnRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
+        super.onCreate(savedInstanceState);
     }
 
     @Override
     protected void initView() {
-
+        DisplayUtils.initBackWithTitle(this,"账户注册");
     }
 
     @Override
