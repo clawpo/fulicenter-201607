1460d57dee7c1dbb51d27a7cd5e42d63e610a789
耿小泰 <clawpo@foxmail.com>
2016-10-24 14:55:08 +0800

    添加shareprefrence保存用户名

--- app/src/main/java/cn/ucai/fulicenter/FuLiCenterApplication.java
+++ app/src/main/java/cn/ucai/fulicenter/FuLiCenterApplication.java
@@ -2,12 +2,15 @@ package cn.ucai.fulicenter;
 
 import android.app.Application;
 
+import cn.ucai.fulicenter.bean.User;
+
 
 public class FuLiCenterApplication extends Application {
     public static FuLiCenterApplication application;
     private static FuLiCenterApplication instance;
 
     private static String username;
+    private static User user;
 
     @Override
     public void onCreate() {
@@ -29,5 +32,13 @@ public class FuLiCenterApplication extends Application {
 
     public static void setUsername(String username) {
         FuLiCenterApplication.username = username;
+    }
+
+    public static User getUser() {
+        return user;
+    }
+
+    public static void setUser(User user) {
+        FuLiCenterApplication.user = user;
     }
 }
--- app/src/main/java/cn/ucai/fulicenter/activity/LoginActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/LoginActivity.java
@@ -10,10 +10,13 @@ import android.widget.EditText;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.ucai.fulicenter.FuLiCenterApplication;
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.bean.Result;
 import cn.ucai.fulicenter.bean.User;
+import cn.ucai.fulicenter.dao.SharePrefrenceUtils;
+import cn.ucai.fulicenter.dao.UserDao;
 import cn.ucai.fulicenter.net.NetDao;
 import cn.ucai.fulicenter.net.OkHttpUtils;
 import cn.ucai.fulicenter.utils.CommonUtils;
@@ -101,7 +104,15 @@ public class LoginActivity extends BaseActivity {
                     if(result.isRetMsg()){
                         User user = (User) result.getRetData();
                         L.e(TAG,"user="+user);
-                        MFGT.finish(mContext);
+                        UserDao dao = new UserDao(mContext);
+                        boolean isSuccess = dao.saveUser(user);
+                        if(isSuccess){
+                            SharePrefrenceUtils.getInstence(mContext).saveUser(user.getMuserName());
+                            FuLiCenterApplication.setUser(user);
+                            MFGT.finish(mContext);
+                        }else{
+                            CommonUtils.showLongToast(R.string.user_database_error);
+                        }
                     }else{
                         if(result.getRetCode()==I.MSG_LOGIN_UNKNOW_USER){
                             CommonUtils.showLongToast(R.string.login_fail_unknow_user);
