f0b82b33c56ebf15fd2f38b6a202ee629da21036
耿小泰 <clawpo@foxmail.com>
2016-10-27 17:40:54 +0800

    购物车的商品删除的操作

--- app/src/main/java/cn/ucai/fulicenter/fragment/CartFragment.java
+++ app/src/main/java/cn/ucai/fulicenter/fragment/CartFragment.java
@@ -167,6 +167,7 @@ public class CartFragment extends BaseFragment {
             mTvCartSavePrice.setText("节省:￥"+Double.valueOf(sumPrice-rankPrice));
 
         }else{
+            setCartLayout(false);
             mTvCartSumPrice.setText("合计:￥0");
             mTvCartSavePrice.setText("节省:￥0");
         }
@@ -181,6 +182,7 @@ public class CartFragment extends BaseFragment {
         public void onReceive(Context context, Intent intent) {
             L.e(TAG,"updateCartReceiver...");
             sumPrice();
+            setCartLayout(mList!=null&&mList.size()>0);
         }
     }
 
